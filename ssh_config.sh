30561366906658faa68f40325a92378cf53d63a5
Deac Karns <fkarns@bandwidth.com>
2022-07-12 11:01:29 -0400

    mkdir keys

--- ssh_config.sh
+++ ssh_config.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+mkdir $HOME/.ssh/keys
+
 lpass show HashSalt.pem --field "Private Key" > $HOME/.ssh/keys/HashSalt.pem
 lpass show karnsonline.pem --field "Private Key" > $HOME/.ssh/keys/karnsonline.pem
