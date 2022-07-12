6c4987202d084e4d8b14b84ae9a0f45e8610bf5e
Deac Karns <fkarns@bandwidth.com>
2022-07-12 11:45:25 -0400

    use for loop and array for key names

--- ssh_config.sh
+++ ssh_config.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+# This script grabs ssh keys stored in lastpass and sets them up on the machine
+
+KEYS=(
+  HashSalt.pem
+  karnsonline.pem
+)
+
+echo "Ensuring $HOME/.ssh/keys directory exists"
 mkdir -p $HOME/.ssh/keys
 
-lpass show HashSalt.pem --field "Private Key" > $HOME/.ssh/keys/HashSalt.pem
-lpass show karnsonline.pem --field "Private Key" > $HOME/.ssh/keys/karnsonline.pem
-
+echo "Setting permisisons for ssh keys"
 chmod -R 700 $HOME/.ssh/keys
+
+for k in "${KEYS[@]}"
+do
+  echo "Creating $HOME/.ssh/keys/$k"
+  lpass show $k --field "Private Key" > $HOME/.ssh/keys/$k
+done
