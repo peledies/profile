a9a227367849dd5326c255d46efcc607b590f9ca
Deac Karns <fkarns@bandwidth.com>
2024-05-15 12:06:18 -0400

    add k9s config sync

---
+++ k9s/config_sync.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+red=$(tput setaf 1)
+default=$(tput sgr0)
+
+# this is the "folder" in lastpass
+NAMESPACE="kubernetes-configs"
+
+IDS=$(lpass show -xjG '.*' | jq --raw-output '.[] | select( .group == "'$NAMESPACE'" ) | select(.url != "http://group" ) | .id')
+
+echo -e "\n${cyan}Creating the following kube config files in ${magenta}$HOME/.kube${cyan}:${default}\n"
+
+for ID in $IDS
+do
+  NAME=$(lpass show $ID --name)
+  printf "%-40s %s\n" "$NAME" "${magenta}$ID${default}"
+  CONFIG="$(lpass show $ID --notes)"
+done
